b2def846f1047299d522c6835d689a9c8e1adea6
Thomas Legrand <thomas.legrand1@gmail.com>
2016-12-05 09:58:06 -0800

    working day 2 part 2

--- day2/keypad_code.py
+++ day2/keypad_code.py
@@ -1,6 +1,7 @@
 import os
 
 KEYPAD = [range(i * 3 + 1, (i + 1) * 3 + 1) for i in range(3)]
+KEYPAD2 = [[1], [2, 3, 4], [5, 6, 7, 8, 9], ["A", "B", "C"], ["D"]]
 MOVES = {
     "U": lambda s: s.move_up(),
     "D": lambda s: s.move_down(),
@@ -37,17 +38,77 @@ class State:
             self.y += 1
 
 
+class State2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move_up(self):
+        if self.y > 2:
+            self.y -= 1
+        elif self.y == 2:
+            if 0 < self.x < 4:
+                self.y -= 1
+        elif self.y == 1:
+            if self.x == 2:
+                self.y -= 1
+
+    def move_left(self):
+        if self.x > 2:
+            self.x -= 1
+        elif self.x == 2:
+            if 0 < self.y < 4:
+                self.x -= 1
+        elif self.x == 1:
+            if self.y == 2:
+                self.x -= 1
+
+    def move_right(self):
+        if self.x < 2:
+            self.x += 1
+        elif self.x == 2:
+            if 0 < self.y < 4:
+                self.x += 1
+        elif self.x == 3:
+            if self.y == 2:
+                self.x += 1
+
+    def move_down(self):
+        if self.y < 2:
+            self.y += 1
+        elif self.y == 2:
+            if 0 < self.x < 4:
+                self.y += 1
+        elif self.y == 3:
+            if self.x == 2:
+                self.y += 1
+
+
+
 def main():
     input_path = os.path.join('input', 'directions')
     print get_input(input_path)
     print [range(i * 3 + 1, (i + 1) * 3 + 1) for i in range(3)]
     input = get_input(input_path)
-    for input_line in input:
-        s = State(0, 0)
-        [MOVES[instruction](s) for instruction in input_line]
-        print s.x, s.y
-        print KEYPAD[s.y][s.x]
+    # for input_line in input:
+    #     s = State(1, 1)
+    #     [MOVES[instruction](s) for instruction in input_line]
+    #     print s.x, s.y
+    #     print KEYPAD[s.y][s.x]
 
+    print input[0]
+    for input_line in input:
+        st = State2(0, 2)
+        for instruction in input_line:
+            MOVES[instruction](st)
+            # print instruction
+            # print st.x, st.y
+        if st.y in [1, 3]:
+            print KEYPAD2[st.y][st.x - 1]
+        elif st.y in [0, 4]:
+            print KEYPAD2[st.y][st.x - 2]
+        else:
+            print KEYPAD2[st.y][st.x]
 
 if __name__ == '__main__':
     main()
