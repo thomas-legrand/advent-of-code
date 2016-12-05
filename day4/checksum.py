ddd92b923945e7629eb95982ac5c4885d5875fd5
Thomas Legrand <thomas.legrand1@gmail.com>
2016-12-05 14:51:20 -0800

    part 2 solved for day4

--- day4/checksum.py
+++ day4/checksum.py
@@ -1,12 +1,12 @@
 import os
 import string
 import collections
+from functools import partial
 
 
 def get_input(input_path):
     directions = open(input_path)
     rows = directions.readlines()
-    print rows
     data = [r.rstrip("]\n").split("[") for r in rows]
     for d in data:
         assert len(d[1]) == 5
@@ -21,19 +21,54 @@ def compute_checksum(s):
     return ''.join([char[0] for char in allowed_chars[:5]])
 
 
-def run_test():
+def decode_char(c, sector_id):
+    letters = string.ascii_lowercase
+    n = len(letters)
+    i = letters.index(c)
+    offset = sector_id % n
+    new_i = (offset + i) % n
+    return letters[new_i]
+
+
+def decrypt_string(s):
+    spl = s.split("-")
+    useful_stuff = spl[:-1]
+    sector_id = int(spl[-1])
+    decode = partial(decode_char, sector_id=sector_id)
+    words = ["".join(map(decode, stuff)) for stuff in useful_stuff]
+    return " ".join(words)
+
+
+def run_test_part1():
     input_path = os.path.join('input', 'test')
     data = get_input(input_path)
     valid_names = [d for d in data if compute_checksum(d[0]) == d[1]]
     assert sum([int(d[2]) for d in valid_names]) == 1514
 
 
+def run_test_decoding():
+    assert decode_char("a", 5) == "f"
+
+
+def run_test_part2():
+    input_string = 'qzmt-zixmtkozy-ivhz-343'
+    actual_string = decrypt_string(input_string)
+    expected_string = 'very encrypted name'
+    assert expected_string == actual_string
+
+
 def main():
-    run_test()
+    run_test_part1()
+    run_test_decoding()
+    run_test_part2()
     input_path = os.path.join('input', 'data')
     data = get_input(input_path)
     valid_names = [d for d in data if compute_checksum(d[0]) == d[1]]
     print sum([int(d[2]) for d in valid_names])
+    decrypted_names = [[decrypt_string(d[0]),d[2]] for d in valid_names]
+    print len(decrypted_names)
+    print decrypted_names
+    print filter(lambda x: "north" in x[0], decrypted_names)
 
 if __name__ == '__main__':
     main()
