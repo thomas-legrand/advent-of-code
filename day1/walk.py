dc86fbd88532d91938c6f27111742000d0918c05
Thomas Legrand <thomas.legrand1@gmail.com>
2016-12-04 17:17:21 -0800

    solved day1 part 2

--- day1/walk.py
+++ day1/walk.py
@@ -1,5 +1,6 @@
 import os
 import re
+import copy
 import numpy as np
 
 # note: a pretty cool solution exists, using complex numbers, at
@@ -27,17 +28,26 @@ class State(object):
     def __str__(self):
         return "position %(p)s - direction %(d)s" % {'p': self.position, 'd': self.direction}
 
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+    def move_one_block(self):
+        self.position = [sum(x) for x in zip(self.position, self.direction)]
+
     def move(self, instruction):
         assert isinstance(instruction, Instruction)
         # first update the direction
         angle = POSSIBLE_TURNS[instruction.turn]
         updated_direction_list = get_new_direction(self.direction, angle)
+        assert updated_direction_list in POSSIBLE_DIRECTIONS
         self.direction = updated_direction_list
+        inter_positions = []
 
-        # then update the position
-        assert self.direction in POSSIBLE_DIRECTIONS
-        change = [d * instruction.number for d in self.direction]
-        self.position = [sum(x) for x in zip(self.position, change)]
+        for i in xrange(instruction.number):
+            self.move_one_block()
+            inter_positions.append(self.position)
+
+        return inter_positions
 
 
 def get_new_direction(direction, angle):
@@ -60,17 +70,31 @@ def fetch_input_list_instructions(input_path):
     return [get_instruction_from_string(s) for s in instructions_string.split(", ")]
 
 
+def compute_position_sequence_from_instructions(initial_state, instructions):
+    visited_positions = [initial_state.position]
+    current_state = copy.copy(initial_state)
+    for instruction in instructions:
+        new_positions = current_state.move(instruction)
+        visited_positions += new_positions
+    return visited_positions
+
+
+def compute_position_visited_twice(visited_positions):
+    for i, position in enumerate(visited_positions):
+        if position in visited_positions[:i]:
+            return position
+
+
 def main():
     input_path = os.path.join('input', 'directions')
     instructions = fetch_input_list_instructions(input_path)
     # start with initial state (x = 0, y = 0) in a 2D plane, and head towards increasing x.
-    a = State([0, 0], [1, 0])
-    print a
-    for i in instructions:
-        print i
-        a.move(i)
-        print a
-    print sum(map(abs, a.position))
+    initial_state = State([0, 0], [1, 0])
+    visited_position = compute_position_sequence_from_instructions(initial_state, instructions)
+    print sum(map(abs, visited_position[-1]))
+    first_visited_twice = compute_position_visited_twice(visited_position)
+    print sum(map(abs, first_visited_twice))
+
 
 
 if __name__ == '__main__':
