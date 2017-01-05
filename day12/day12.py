b84e849990b64311485b15929ab70c81e37ef69f
Thomas Legrand <thomas.legrand1@gmail.com>
2017-01-04 19:46:41 -0800

    day12 updated after day23 build up

--- day12/day12.py
+++ day12/day12.py
@@ -1,65 +1,66 @@
-def get_input(input_path):
-    directions = open(input_path)
-    rows = directions.readlines()
-    return [r.strip("\n") for r in rows]
+from collections import defaultdict
+
+
+class Assembunny(object):
+    def __init__(self, input_path):
+        directions = open(input_path)
+        rows = directions.readlines()
+        self.instructions = [r.strip("\n") for r in rows]
+        self.register = defaultdict(int)
+
+    def copy(self, value, target):
+        self.register[target] = self.register[value] if value in self.register else int(value)
+
+    def inc(self, target):
+        self.register[target] += 1
+
+    def dec(self, target):
+        self.register[target] -= 1
+
+    def jnz(self, value, skip):
+        try:
+            int(value)
+            return int(skip)
+        except:
+            return int(skip) if self.register[value] != 0 else 1
+
+    def execute_instructions(self):
+        i = 0
+        while i < len(self.instructions):
+            current_instruction = self.instructions[i]
+            split_instruction = current_instruction.split(" ")
+            if split_instruction[0] == "cpy":
+                self.copy(split_instruction[1], split_instruction[2])
+                i += 1
+            elif split_instruction[0] == "inc":
+                self.inc(split_instruction[1])
+                i += 1
+            elif split_instruction[0] == "dec":
+                self.dec(split_instruction[1])
+                i += 1
+            elif split_instruction[0] == "jnz":
+                i += self.jnz(split_instruction[1], split_instruction[2])
 
 
 def run_test():
-    register = {}
-    instructions = get_input("test")
-    execute_instructions(instructions, register)
-    assert register['a'] == 42
-
-
-def execute_instructions(instructions, register):
-    i = 0
-    while i < len(instructions):
-        current_instruction = instructions[i]
-        split_instruction = current_instruction.split(" ")
-        if split_instruction[0] == "cpy":
-            if split_instruction[1] in register:
-                register[split_instruction[2]] = register[split_instruction[1]]
-            else:
-                register[split_instruction[2]] = int(split_instruction[1])
-            i += 1
-        elif split_instruction[0] == "inc":
-            if split_instruction[1] not in register:
-                register[split_instruction[1]] = 1
-            else:
-                register[split_instruction[1]] += 1
-            i += 1
-        elif split_instruction[0] == "dec":
-            if split_instruction[1] not in register:
-                register[split_instruction[1]] = -1
-            else:
-                register[split_instruction[1]] -= 1
-            i += 1
-        elif split_instruction[0] == "jnz":
-            if split_instruction[1] in register and register[split_instruction[1]] != 0:
-                i += int(split_instruction[2])
-            else:
-                try:
-                    int(split_instruction[1])
-                    i += int(split_instruction[2])
-                except Exception as e:
-                    register[split_instruction[1]] = 0
-                    i += 1
+    a = Assembunny("test")
+    a.execute_instructions()
+    assert a.register['a'] == 42
 
 
 def main():
     run_test()
-    instructions = get_input("input")
 
     # part 1
-    register = {}
-    execute_instructions(instructions, register)
-    print(register['a'])
+    a = Assembunny("input")
+    a.execute_instructions()
+    print(a.register['a'])
 
     # part 2
-    register = {'c': 1}
-    execute_instructions(instructions, register)
-    print(register['a'])
-
+    a = Assembunny("input")
+    a.register['c'] = 1
+    a.execute_instructions()
+    print(a.register['a'])
 
 if __name__ == '__main__':
     main()
