b639afaf1e30e4186be6f82c67edcdf5fda6c24e
Thomas Legrand <thomas.legrand1@gmail.com>
2017-12-01 14:31:07 -0800

    day01 2017 - rank ~240

---
+++ 2017/day01/day01.py
@@ -0,0 +1,44 @@
+def get_input():
+    f = open('input')
+    return f.readline()
+
+
+def sum_next(s):
+    i = 0
+    c = 0
+    while i < len(s):
+        ind = (i + 1) % len(s)
+        if s[i] == s[ind]:
+            c += int(s[i])
+        i += 1
+    return c
+
+
+def sum_next2(s):
+    i = 0
+    c = 0
+    while i < len(s):
+        ind = (i + len(s) / 2) % len(s)
+        if s[i] == s[ind]:
+            c += int(s[i])
+        i += 1
+    return c
+
+
+def run_test():
+    assert sum_next('1122') == 3
+    assert sum_next('1111') == 4
+    assert sum_next('1234') == 0
+    assert sum_next2('1212') == 6
+    assert sum_next2('1221') == 0
+    assert sum_next2('123425') == 4
+
+
+def main():
+    run_test()
+    inp = get_input()
+    print sum_next(inp)
+    print sum_next2(inp)
+
+if __name__ == '__main__':
+    main()
