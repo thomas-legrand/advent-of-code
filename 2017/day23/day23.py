c0c64403433d5fdba507f14be3f87bafb5fe1e84
Thomas Legrand <thomas.legrand1@gmail.com>
2018-05-24 21:59:19 -0700

    decompiled assembly for day23 part 2

--- 2017/day23/day23.py
+++ 2017/day23/day23.py
@@ -10,9 +10,10 @@ JNZ = 'jnz'
 
 
 class Solution(object):
-    def __init__(self, instructions):
+    def __init__(self, instructions, initial_value_a):
         self.instructions = instructions
         self.register = defaultdict(int)
+        self.register['a'] = initial_value_a
 
     def read(self):
         def calc(v):
@@ -37,12 +38,36 @@ class Solution(object):
             i += 1
         return count_mul
 
+    def read2(self):
+        h = 0
+        b = 0
+        c = 0
+        if self.register['a'] != 0:
+            b = 81 * 100 + 100000
+            c = b + 17000
+        while True:
+            f = 1
+            max_v = b
+            for d in range(2, b):
+                if b % d == 0:
+                    f = 0
+                if d > max_v:
+                    break
+                max_v = b / d
+            if f == 0:
+                h += 1
+            if b == c:
+                return h
+            b += 17
+
 
 def main():
     # run_test()
     inp = iu.get_multi_line_input('input')
-    s = Solution(inp)
+    s = Solution(inp, 0)
     print s.read()
+    s = Solution(inp, 1)
+    print s.read2()
 
 if __name__ == '__main__':
     main()
